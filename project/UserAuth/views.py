63d75439034789db379012d4cb012843d1b3cd32
MrIncxyl <tsktechnical7@gmail.com>
2026-03-13 02:08:31 +0530

    Register POST GET

--- project/UserAuth/views.py
+++ project/UserAuth/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def Login(request):
     return render(request,'login.html')
 
 def Register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('useremail')
+        password = request.POST.get('password')
+        
+        alreadyExist = User.objects.filter(username=username).exists()
+        
+        if alreadyExist:
+            messages.error(request,'Username already exist')
+            return render(request,'register.html')
+        else:
+            new_user = User.objects.create_user(username=username,password=password,email=email)
+            new_user.save()
+            messages.success(request,'User registered successfully🦋')
+            return render(request,'login.html')
+        
     return render(request,'register.html')
