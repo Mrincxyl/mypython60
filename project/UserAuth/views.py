776cd47302ada35c44c06c0dbff044e598a361df
MrIncxyl <tsktechnical7@gmail.com>
2026-03-26 23:07:38 +0530

    Login function

--- project/UserAuth/views.py
+++ project/UserAuth/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+
+
 def Login(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        if not username or not password:
+            messages.error(request, 'All fields are required')
+            return redirect('login')
+        
+        user = authenticate(request,username = username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,'Logged in Successfully')
+            return redirect('home')
+        else:
+            messages.error(request,'Invalid username or password')
+            return redirect('login')
+            
+        
     return render(request,'login.html')
 
 def Register(request):
