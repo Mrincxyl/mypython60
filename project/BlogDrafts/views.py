4608ce3ecec773cf75978cebb8bf44f1fb70cbc6
MrIncxyl <tsktechnical7@gmail.com>
2026-04-09 22:01:08 +0530

    In html: category dropdown, slug, visibility, In views.py: add_blog function crea

--- project/BlogDrafts/views.py
+++ project/BlogDrafts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required 
 from django.contrib import messages
 from .models import Category, Blog
+import uuid
+from django.utils.text import slugify
+
 
 # Create your views here.
 
@@ -27,7 +30,54 @@ def Create(request):
                          
         # handle create blog
         if 'add_blog' in request.POST:
-            pass    
+            
+            title =  request.POST.get('title')  
+            slug = request.POST.get('slug')
+            
+            author = request.user
+            category_id = request.POST.get('category')
+            
+            image = request.FILES.get('thumbnail')
+            content = request.POST.get('content')
+            
+            visibility = request.POST.get('visibility')
+            
+            
+            if not title or not content:
+                messages.error(request,"Title and Content are required!")
+                return redirect('create')
+            
+            
+            category = None
+            if category_id:
+                try:
+                    category = Category.objects.get(id=category_id)
+                    
+                except Category.DoesNotExist:
+                    messages.error(request,"Invalid category selected!")
+                    return redirect('create')  
+                
+            if not slug:      
+                slug = slugify(title) + "-" + str(uuid.uuid4())[:4]
+                
+                
+            if Blog.objects.filter(slug=slug).exists():
+                slug = slug + "-"  +  str(uuid.uuid4())[:3]
+            
+            Blog.objects.create(
+                title = title,
+                slug=slug,
+                author=author,
+                category = category,
+                image = image,
+                content = content,
+                visibility = visibility
+            )
+            
+            messages.success(request, 'Blog Uploaded Successfully.')
+            return redirect('create')
+            
+            
             
         if  'delete_category' in request.POST:
             id = request.POST.get('delete_category') 
